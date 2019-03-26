289d0ac1bca324bc60932f3be0e07ed9459bab9d
Bryce Morgan <bamorgan13@gmail.com>
2019-03-26 13:04:04 -0400

    Implement basic int set and hashing methods

--- HashMap/lib/p01_int_set.rb
+++ HashMap/lib/p01_int_set.rb
@@ -1,22 +1,30 @@
 class MaxIntSet
   def initialize(max)
+    @max = max
+    @store = Array.new(max, false)
   end
 
   def insert(num)
+    validate!(num)
+    @store[num] = true
   end
 
   def remove(num)
+    @store[num] = false
   end
 
   def include?(num)
+    @store[num]
   end
 
   private
 
   def is_valid?(num)
+    num < @max && num >= 0
   end
 
   def validate!(num)
+    raise "Out of bounds" unless is_valid?(num)
   end
 end
 
@@ -27,20 +35,24 @@ class IntSet
   end
 
   def insert(num)
+    self[num] << num
   end
-
+  
   def remove(num)
+    self[num].delete(num)
   end
-
+  
   def include?(num)
+    self[num].include?(num)
   end
-
+  
   private
-
+  
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
-
+  
   def num_buckets
     @store.length
   end
@@ -48,25 +60,32 @@ end
 
 class ResizingIntSet
   attr_reader :count
-
+  
   def initialize(num_buckets = 20)
     @store = Array.new(num_buckets) { Array.new }
     @count = 0
   end
-
+  
   def insert(num)
+    return if self.include?(num)
+    resize! if self.count == num_buckets
+    self[num] << num
+    @count += 1
   end
-
+  
   def remove(num)
+    @count -= 1 if self[num].delete(num) 
   end
-
+  
   def include?(num)
+    self[num].include?(num)
   end
-
+  
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -74,5 +93,13 @@ class ResizingIntSet
   end
 
   def resize!
+    new_size = num_buckets * 2
+    bigger_store = Array.new(new_size) {Array.new}
+    @store.each do |bucket|
+      bucket.each do |el|
+        bigger_store[el % new_size] << el
+      end
+    end
+    @store = bigger_store
   end
 end
--- HashMap/lib/p02_hashing.rb
+++ HashMap/lib/p02_hashing.rb
@@ -4,11 +4,17 @@ end
 
 class Array
   def hash
+    entangled = self.map.with_index {|el, idx| el * (idx + 3)}
+    compressed = entangled.sum
+    compressed.hash
   end
 end
 
 class String
   def hash
+    chars = self.chars
+    chars_to_ints = chars.map {|char| char.ord}
+    chars_to_ints.hash
   end
 end
 
@@ -16,6 +22,7 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    hashed_pairs = self.map {|k, v| (k.to_s.hash + v.hash).hash}
+    hashed_pairs.sum.hash
   end
 end
