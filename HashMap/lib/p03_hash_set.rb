5ecec1df87d0723441b4d444a1877f19b67bb92e
Bryce Morgan <bamorgan13@gmail.com>
2019-03-26 13:13:34 -0400

    Implement HashSet to work with any data type

--- HashMap/lib/p03_hash_set.rb
+++ HashMap/lib/p03_hash_set.rb
@@ -7,18 +7,25 @@ class HashSet
   end
 
   def insert(key)
+    return if self.include?(key)
+    resize! if self.count == num_buckets
+    self[key.hash] << key
+    @count += 1
   end
 
   def include?(key)
+    self[key.hash].include?(key)
   end
 
   def remove(key)
+    @count -= 1 if self[key.hash].delete(key) 
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -26,5 +33,13 @@ class HashSet
   end
 
   def resize!
+    new_size = num_buckets * 2
+    bigger_store = Array.new(new_size) {Array.new}
+    @store.each do |bucket|
+      bucket.each do |el|
+        bigger_store[el % new_size] << el
+      end
+    end
+    @store = bigger_store
   end
 end
